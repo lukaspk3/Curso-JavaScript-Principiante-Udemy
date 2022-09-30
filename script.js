3c6f4560e7eb6eacafa60b144a1ba81e6f4ff403
Lukas <106382202+lukaspk3@users.noreply.github.com>
2022-09-30 19:10:22 -0300

    Ejercicio 2
    Modificando y Enlanzando HTML con JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementsByClassName("parrafo2").innerHTML= "Parrafo 2 modificado con JS";
+
+document.getElementById("parrafo3").innerHTML= "Parrafo 3 modificado con JS";
+
+document.getElementsByClassName(".parrafo2").innerHTML= "Parrafo 2 modificado con JS";
