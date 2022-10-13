7d63fb87a5953570338a774534e91691c3744308
Lukas <106382202+lukaspk3@users.noreply.github.com>
2022-10-13 15:54:52 -0300

    Ejercicio nº3

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-/* 
-// Modificando contenido con JavaScript
-document.querySelector(`h1`).innerHTML= `Ejercicio Curso JS, modificacion de HTML con JS <hr>`;
-
-document.querySelector(`p`).textContent= `Parrafo 1, modificado utilizando JS`;
-
-document.querySelector('.parrafo2').innerHTML= "Parrafo 2 modificado con JS";
-
-document.getElementById("parrafo3").innerText= "Parrafo 3 modificado con JS";
-
-//Modificando Clases con JavaScript
-document.querySelector(`p`).setAttribute(`Class`, `Green`);
-document.querySelector(`.parrafo2`).setAttribute(`Class`, `Orange`);
- */
-
-let Array = [10,5,23,20];
-console.log(Array);
-Array=(11);
-console.log(Array);
+function multiplicar(){
+	baseImponible = document.getElementById("BaseImponible").value;
+	iva = document.getElementById("Iva").value;
+	resultado = parseFloat(baseImponible)+ parseFloat(baseImponible*(iva/100));
+	document.getElementById("Total").value = resultado;
+	}
