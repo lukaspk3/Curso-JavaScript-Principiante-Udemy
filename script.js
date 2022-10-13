f5c2209f6406a46673d92c03d889653396951de0
Lukas <106382202+lukaspk3@users.noreply.github.com>
2022-10-13 10:24:16 -0300

    Ejercicio 2 Finalizado

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+/* 
+// Modificando contenido con JavaScript
+document.querySelector(`h1`).innerHTML= `Ejercicio Curso JS, modificacion de HTML con JS <hr>`;
 
-document.getElementsByClassName("parrafo2").innerHTML= "Parrafo 2 modificado con JS";
+document.querySelector(`p`).textContent= `Parrafo 1, modificado utilizando JS`;
 
-document.getElementById("parrafo3").innerHTML= "Parrafo 3 modificado con JS";
+document.querySelector('.parrafo2').innerHTML= "Parrafo 2 modificado con JS";
 
-document.getElementsByClassName(".parrafo2").innerHTML= "Parrafo 2 modificado con JS";
+document.getElementById("parrafo3").innerText= "Parrafo 3 modificado con JS";
+
+//Modificando Clases con JavaScript
+document.querySelector(`p`).setAttribute(`Class`, `Green`);
+document.querySelector(`.parrafo2`).setAttribute(`Class`, `Orange`);
+ */
+
+let Array = [10,5,23,20];
+console.log(Array);
+Array=(11);
+console.log(Array);
